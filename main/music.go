83841f99aad6b0f0bfaf22b4075d4dedb35444a9
rENC36 <hellferry5@gmail.com>
2026-07-11 21:20:39 +0300

    updated

--- main/music.go
+++ main/music.go
@@ -7,5 +7,18 @@ embedded_components {
   type: "sound"
   data: "sound: \"/Assets/sounds/menu_music.ogg\"\n"
   "looping: 1\n"
+  "gain: 0.4\n"
+  ""
+}
+embedded_components {
+  id: "sound_click"
+  type: "sound"
+  data: "sound: \"/Assets/sounds/click_sound.ogg\"\n"
+  ""
+}
+embedded_components {
+  id: "sound_hover"
+  type: "sound"
+  data: "sound: \"/Assets/sounds/hover_sound.ogg\"\n"
   ""
 }
