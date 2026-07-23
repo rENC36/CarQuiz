d428fd8f1ce355c5a453e5e31acd905054c91db9
J0szs <devthings.star@gmail.com>
2026-07-23 13:30:22 +0300

    Added a menu

--- main/music.go
+++ main/music.go
@@ -5,7 +5,7 @@ components {
 embedded_components {
   id: "sound"
   type: "sound"
-  data: "sound: \"/Assets/sounds/menu_music.ogg\"\n"
+  data: "sound: \"/Assets/sounds/menu_sound.ogg\"\n"
   "looping: 1\n"
   "gain: 0.4\n"
   ""
@@ -20,5 +20,41 @@ embedded_components {
   id: "sound_hover"
   type: "sound"
   data: "sound: \"/Assets/sounds/hover_sound.ogg\"\n"
+  ""
+}
+embedded_components {
+  id: "sound_gameplay"
+  type: "sound"
+  data: "sound: \"/Assets/sounds/gameplay_sound.ogg\"\n"
+  ""
+}
+embedded_components {
+  id: "sound_timer"
+  type: "sound"
+  data: "sound: \"/Assets/sounds/timer_sound.ogg\"\n"
+  ""
+}
+embedded_components {
+  id: "sound_correct"
+  type: "sound"
+  data: "sound: \"/Assets/sounds/correct_sound.ogg\"\n"
+  ""
+}
+embedded_components {
+  id: "sound_wrong"
+  type: "sound"
+  data: "sound: \"/Assets/sounds/wrong_sound.ogg\"\n"
+  ""
+}
+embedded_components {
+  id: "sound_win"
+  type: "sound"
+  data: "sound: \"/Assets/sounds/win_sound.ogg\"\n"
+  ""
+}
+embedded_components {
+  id: "sound_defeat"
+  type: "sound"
+  data: "sound: \"/Assets/sounds/defeat_sound.ogg\"\n"
   ""
 }
