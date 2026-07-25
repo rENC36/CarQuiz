38e3ac852b3ce2428e85168b47861d2cf08c89cb
J0szs <devthings.star@gmail.com>
2026-07-25 12:34:31 +0300

    localisation

--- scripts/managers/localization_manager.lua
+++ scripts/managers/localization_manager.lua
@@ -1,34 +1,66 @@
+-- scripts/managers/localization_manager.lua
 local M = {}
 
-local dicts = {
+local locales = {
 	ru = require("localization.strings_ru"),
 	en = require("localization.strings_en"),
 }
 
-M.current_lang = "ru" 
+M.current_lang = "ru"
 
-function M.init(lang, place)
-	if lang and dicts[lang] then
+function M.init(lang, screen_name)
+	if lang then
 		M.current_lang = lang
-	else
-		M.current_lang = "ru"
 	end
-	
-	M.set_lang(M.current_lang, place)
+
+	local locale = locales[M.current_lang]
+	if not locale then
+		M.current_lang = "ru"
+		locale = locales[M.current_lang]
+		if not locale then return end
+	end
+
+	if screen_name then
+		M.update_screen(screen_name, locale)
+	end
 end
 
-function M.get(key)
-	local dict = dicts[M.current_lang]
-	return dict[key] or dicts["ru"][key] or key
-end
+function M.update_screen(screen_name, locale)
+	local texts = locale[screen_name]
+	if not texts then return end
 
-function M.set_lang(lang, place)
-	if dicts[lang] then
-		M.current_lang = lang
-
-		for key, text in pairs(dicts[lang][place]) do
-			gui.set_text(gui.get_node(key), text)
+	for key, value in pairs(texts) do
+		local node = gui.get_node(key)
+		if node then
+			gui.set_text(node, value)
 		end
+	end
+end
+
+function M.get_text(key)
+	local locale = locales[M.current_lang]
+	if not locale then return key end
+
+	local parts = {}
+	for part in string.gmatch(key, "[^%.]+") do
+		table.insert(parts, part)
+	end
+
+	local current = locale
+	for _, part in ipairs(parts) do
+		if current[part] then
+			current = current[part]
+		else
+			return key
+		end
+	end
+
+	return current
+end
+
+function M.set_lang(lang)
+	if lang then
+		M.current_lang = lang
 	end
 end
 
